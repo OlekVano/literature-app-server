48193ef0154e6cdd6b103c0eab20ae87e332c22b
OlekVano <olekvano@gmail.com>
2022-12-26 15:31:27 +0000

    Added 3 functions to the utils.ts file: getRandArrayItem, replaceAll and fillPattern

--- utils.ts
+++ utils.ts
@@ -12,3 +12,19 @@ export function getRandNumArray(max: number, length: number): number[] {
   }
   return arr
 }
+
+export function getRandArrayItem<T>(arr: T[]): T {
+  return arr[getRandNum(arr.length)]
+}
+
+export function replaceAll(str: string, pattern: string, replacement: string): string {
+  return str.split(pattern).join(replacement)
+}
+
+export function fillPattern(questionPattern: string, dict: {[key: string]: string}): string {
+  const keys: string[] = Object.keys(dict)
+
+  keys.forEach(key => questionPattern = replaceAll(questionPattern, `%${key}%`, dict[key]))
+
+  return questionPattern
+}
