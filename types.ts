e66ed3c827568b6c6b8b015aa549240df9451587
OlekVano <olekvano@gmail.com>
2023-01-10 08:50:33 +0000

    Added WriterProperty and AskableWriterProperty types to types.ts

--- types.ts
+++ types.ts
@@ -1,4 +1,7 @@
-import { Work } from "@prisma/client";
+import { Work, Writer } from "@prisma/client";
 
 export type WorkProperty = keyof Work
 export type AskableWorkProperty = Exclude<WorkProperty, 'id' | 'country' | 'year' | 'age' | 'name'>
+
+export type WriterProperty = keyof Writer
+export type AskableWriterProperty = Exclude<WriterProperty, 'id' | 'country' | 'years' | 'name'>
