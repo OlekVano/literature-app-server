4a42cd70df9e1a73bacfa0e11a25163c05975ab3
OlekVano <olekvano@gmail.com>
2023-01-10 09:21:12 +0000

    Added getRandQuestion, getRandABCDQuestion, getRandPropVarsExcept and getAllPropVariations function to the writers.ts file

--- writers.ts
+++ writers.ts
@@ -1,7 +1,8 @@
 import { Writer } from '@prisma/client'
 import prisma from './db'
+import { AskableWriterProperty } from './types'
 
-import { getRandNum } from './utils'
+import { fillPattern, getKeysValues, getRandArrItem, getRandArrItems, getRandNum, isEmpty, isString, removeAllMatches, removeDuplicates, removeEmpty, shuffleArray } from './utils'
 
 export async function getAllIds(): Promise<string[]> {
   return (await prisma.writer.findMany({
@@ -20,4 +21,57 @@ export async function getRand(): Promise<Writer> {
   return await prisma.writer.findFirstOrThrow({
     skip: skip
   })
+}
+
+async function getAllPropVariations(property: AskableWriterProperty): Promise<string[]> {
+  const selectDict: {[key: string]: boolean} = {}
+  selectDict[property] = true
+  return (await prisma.writer.findMany({
+    select: selectDict
+  })).map(e => e[property]).flat()
+}
+
+async function getRandPropVarsExcept(property: AskableWriterProperty, n: number, exception: string|null = null): Promise<string[]> {
+  const variations: string[] = await getAllPropVariations(property)
+  const uniqueVariations: string[] = removeAllMatches(removeDuplicates(removeEmpty(variations)), exception) as string[]
+  return getRandArrItems(uniqueVariations, n)
+}
+
+export async function getRandABCDQuestion() {
+  const possibleQuestionPatterns: {[key in AskableWriterProperty]: string} = {
+    'pseudonym': 'Під яким псевдонімом відомий "%name%"?',
+    'photoUrl': 'На якому з портретів зображений %name%?',
+    'works': 'Який з наступних творів написав "%name%"?'
+  }
+
+  const randWriter: Writer = await getRand()
+
+  const propertiesPatters: [AskableWriterProperty, string][] = getKeysValues(possibleQuestionPatterns)
+
+  const randomQuestionPattern: [AskableWriterProperty, string] = getRandArrItem(getKeysValues(possibleQuestionPatterns).filter(e => !isEmpty(randWriter[e[0]])))
+
+  const property: AskableWriterProperty = randomQuestionPattern[0]
+  const questionPattern: string = randomQuestionPattern[1]
+
+  const correctAnswer: string = isString(randWriter[property]) ? randWriter[property] as string : getRandArrItem(randWriter[property] as string[])
+
+  const answerOptions = await getRandPropVarsExcept(property, 3, correctAnswer)
+  answerOptions.push(correctAnswer)
+  shuffleArray(answerOptions)
+
+  const question: string = fillPattern(questionPattern, randWriter)
+
+  return {
+    question: question,
+    options: answerOptions,
+    answer: correctAnswer
+  }
+}
+
+export async function getRandQuestion() {
+  const questionFunctions = [
+    getRandABCDQuestion
+  ]
+
+  return await getRandArrItem(questionFunctions)()
 }
